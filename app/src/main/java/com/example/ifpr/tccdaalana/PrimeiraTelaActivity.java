ef5da8b4025d12de9ab377f19f76607dcf60b9fa
nbc <cardosoalana770@gmail.com>
2019-09-06 17:00:45 -0300

    arrumar tela de informacao

--- app/src/main/java/com/example/ifpr/tccdaalana/PrimeiraTelaActivity.java
+++ app/src/main/java/com/example/ifpr/tccdaalana/PrimeiraTelaActivity.java
@@ -1,4 +1,17 @@
 package com.example.ifpr.tccdaalana;
 
-public class PrimeiraTelaActivity {
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class PrimeiraTelaActivity extends Activity {
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_primeira_tela);
+    }
+    public void telaInicio (View view){
+        Intent intent = new Intent(this, LoginActivity.class);
+        startActivity(intent);
+    }
 }
